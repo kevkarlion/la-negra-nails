e3955ca04c5cc3d95ec0499aa1374c76579663b4
Kevin Riquelme <kriquelme10@gmail.com>
2024-11-22 12:18:21 -0300

    testing icon star service

--- src/components/shared/ServiceCard/ServiceCard.tsx
+++ src/components/shared/ServiceCard/ServiceCard.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React from "react";
 
 import { HiOutlineStar } from "react-icons/hi2";
 
-
 interface ServiceCardProps {
-  
   title: string;
   description: string;
-  propertyClassName?: string
+  propertyClassName?: string;
 }
 
-export const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, propertyClassName}) => {
+export const ServiceCard: React.FC<ServiceCardProps> = ({
+  title,
+  description,
+  propertyClassName,
+}) => {
   return (
-    <div className={`${propertyClassName} rounded-lg  flex my-3 justify-center flex-col items-center bg-white  p-6 w-full max-w-sm text-center   h-auto border-secondary-deepPink border-2`}>
+    <div
+      className={`${propertyClassName} rounded-lg  flex my-3 justify-center flex-col items-center bg-white  p-6 w-full max-w-sm text-center   h-auto border-secondary-deepPink border-2`}
+    >
       {/* Logo */}
-      <div className="flex justify-center items-center mb-2 bg-black rounded-full w-14 h-14">
-        <HiOutlineStar size={24}/>
+      <div className="flex z-10 relative justify-center items-center mb-2 bg-black rounded-full w-14 h-14">
+        <HiOutlineStar size={24} className="z-40 relative"/>
       </div>
-      
+
       {/* Título del servicio */}
       <h3 className="text-xl font-semibold mb-2 text-secondary">{title}</h3>
-      
+
       {/* Descripción */}
       <p className="w-2/3 text-black font-light  text-sm z-20">{description}</p>
     </div>
   );
 };
-
